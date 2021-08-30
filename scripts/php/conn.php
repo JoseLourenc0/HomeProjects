46bf3126ae7b447cdfeb7fb77139b6c9b0465b52
joselourenco <3s3phinxil@gmail.com>
2021-08-30 16:06:18 -0300

    empty dbsettings file

--- scripts/php/conn.php
+++ scripts/php/conn.php
@@ -1,5 +1,5 @@
 <?php 
-
+    
     require_once 'dbsettings.php'; //! HERE THE FILE dbsettings.php brings dbhost, dbname, dbusrname and dbusrpass variables to use on PDO Connection
 
     try{
---
+++ scripts/php/dbsettings.php
@@ -0,0 +1,6 @@
+<?php
+
+    $dbhost = '';
+    $dbname = '';
+    $dbusrname = '';
+    $dbusrpass = '';
