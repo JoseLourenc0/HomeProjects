d0d069f5776bb2f9575769600dc40d568e0e0754
joselourenco <3s3phinxil@gmail.com>
2021-09-03 10:47:31 -0300

    Fixing some problems

--- scripts/js/homeactivities/index.js
+++ scripts/js/homeactivities/index.js
@@ -1,86 +1,77 @@
-$.ajax({
-    url:'../../scripts/php/homeactivities/getlastactivity.php',
-    method:'GET',
-    dataType:'json'
-}).done(e=>{
+let root = document.getElementById('root')
 
-    let root = document.getElementById('root')
+function buildSkeleton(){
+    root.innerHTML = 
+    `
+    <div class = "text-light" id = "LastActivity"></div>
+    `
+}
 
-    if(e){
+function getLastActivity(){
+    $.ajax({
+        url:'../../scripts/php/homeactivities/getlastactivity.php',
+        method:'GET',
+        dataType:'json'
+    }).done(e=>{
 
-        root.innerHTML += `
-        <h3 class="text-light" align="center">
-            Last Activity by: ${e.usr_reg} at ${e.date_reg}
-        </h3>
-        <br>
-        `
+        let lastActivity = document.getElementById('LastActivity')
 
-    }else{
+        lastActivity.innerHTML = ''
 
-        root.innerHTML += 
-        `
-        <h3 class="text-light" align="center">
-            Last Activity by: No one at Never
-        </h3>
-        <br>
-        `
+        if(e){
 
-    }
+            lastActivity.innerHTML += `
+            <h3 class="text-light" align="center">
+                Last Activity by: ${e.usr_reg} at ${e.date_reg}
+            </h3>
+            <br>
+            `
 
-})
+        }else{
+
+            lastActivity.innerHTML += 
+            `
+            <h3 class="text-light" align="center">
+                Last Activity by: No one at Never
+            </h3>
+            <br>
+            `
+
+        }
+
+    })
+
+}
+
 
 let arr = new Array()
 
-$.ajax({
+function getUsers(){
+    $.ajax({
 
-    url:'../../scripts/php/homeactivities/getusers.php',
-    method:'GET',
-    dataType:'json'
+        url:'../../scripts/php/homeactivities/getusers.php',
+        method:'GET',
+        dataType:'json'
 
-}).done(e=>{
-    
-    if(e){
+    }).done(e=>{
+        
+        if(e){
 
-        e.forEach((element,i) => {
-            arr[i] = element.name
-        })
+            e.forEach((element,i) => {
+                arr[i] = element.name
+            })
 
-        let root = document.getElementById('root')
+            root.innerHTML += '<section id = "section-creation">'
 
-        root.innerHTML += '<section id = "section-creation">'
+            let section = document.getElementById('section-creation')
 
-        let section = document.getElementById('section-creation')
+            if(typeof arr !== 'undefined'){
 
-        if(typeof arr !== 'undefined'){
+                if(arr.length%2===0){
 
-            if(arr.length%2===0){
-
-                for(let i = 0 ; i < arr.length ; i+=2){
-                    
-                    let u = i + 1
-
-                    section.innerHTML += 
-                        `<div class = "even">
-                            <input onclick = newReg('${arr[i]}') animation = "left" type = "button" id = "${arr[i]}_element" value = "${arr[i]}">
-                            <input onclick = newReg('${arr[u]}') animation = "right" type = "button" id = "${arr[u]}_element" value = "${arr[u]}">
-                        </div>
-                        <br>
-                        `
-                    
-                }
-
-                section.innerHTML += '</section>'
-
-            }else{
-
-                if(arr.length!==1){
-                    
                     for(let i = 0 ; i < arr.length ; i+=2){
                         
                         let u = i + 1
-
-                        if(arr.length-i===1)
-                            break
 
                         section.innerHTML += 
                             `<div class = "even">
@@ -91,21 +82,45 @@ $.ajax({
                             `
                         
                     }
+
+                    section.innerHTML += '</section>'
+
+                }else{
+
+                    if(arr.length!==1){
+                        
+                        for(let i = 0 ; i < arr.length ; i+=2){
+                            
+                            let u = i + 1
+
+                            if(arr.length-i===1)
+                                break
+
+                            section.innerHTML += 
+                                `<div class = "even">
+                                    <input onclick = newReg('${arr[i]}') animation = "left" type = "button" id = "${arr[i]}_element" value = "${arr[i]}">
+                                    <input onclick = newReg('${arr[u]}') animation = "right" type = "button" id = "${arr[u]}_element" value = "${arr[u]}">
+                                </div>
+                                <br>
+                                `
+                            
+                        }
+                    }
+
+                    section.innerHTML += 
+                            `<div class = "odd">
+                                <input onclick = newReg('${arr[arr.length-1]}') animation = "top" type = "button" id = "${arr[arr.length-1]}_element" value = "${arr[arr.length-1]}">
+                            </div>
+                            <br>
+                            `
+
+                    section.innerHTML += '</section>'
                 }
 
-                section.innerHTML += 
-                        `<div class = "odd">
-                            <input onclick = newReg('${arr[arr.length-1]}') animation = "top" type = "button" id = "${arr[arr.length-1]}_element" value = "${arr[arr.length-1]}">
-                        </div>
-                        <br>
-                        `
-
-                section.innerHTML += '</section>'
             }
-
         }
-    }
-})
+    })
+}
 
 function newReg(usr){
     if(typeof usr !== 'undefined'){
@@ -119,10 +134,13 @@ function newReg(usr){
 
         }).done(function(e){
 
-            console.log(e)
+            getLastActivity()
 
         })
 
     }
 }
 
+buildSkeleton()
+getLastActivity()
+getUsers()
